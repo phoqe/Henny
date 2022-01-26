1ff6d1f37316a3026113a4eebf8bb4a34108dde9
Linus Långberg <git@phoqe.dev>
2022-01-26 22:16:45 +0100

    Revert "make story type iterable and identifiable"
    This reverts commit 9c482efec66c1c3298bcdc7c48c95c9ed112427b.

--- Sources/Henny/Models/HNStoryType.swift
+++ Sources/Henny/Models/HNStoryType.swift
@@ -2,15 +2,11 @@ import Foundation
 
 /// The different types of stories there are on Hacker News.
 /// You can use this enum to query the different endpoints.
-public enum HNStoryType: String, Codable, CaseIterable, Identifiable {
+public enum HNStoryType: String, Codable {
     case top
     case new
     case best
     case ask
     case show
     case job
-
-    public var id: String {
-        self.rawValue
-    }
 }
