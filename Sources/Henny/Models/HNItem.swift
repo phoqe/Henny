754a4446afde4572055702c6d8b9bec80a391dbb
Linus Långberg <git@phoqe.dev>
2022-01-26 22:47:16 +0100

    make more public

--- Sources/Henny/Models/HNItem.swift
+++ Sources/Henny/Models/HNItem.swift
@@ -12,100 +12,100 @@ public struct HNItem: Codable, Identifiable {
 
     /// Whether the item is deleted.
     /// It can be deleted by administrators and anti-abuse software.
-    let deleted: Bool?
+    public let deleted: Bool?
 
     /// The type of item.
     /// See `HNItemType` for more info.
-    let type: HNItemType?
+    public let type: HNItemType?
 
     /// The username of the author.
-    let by: String?
+    public let by: String?
 
     /// The date the item was created.
-    let time: Date?
+    public let time: Date?
 
     /// The text content of a story.
     /// It's in HTML.
-    let text: String?
+    public let text: String?
 
     /// Whether the item is dead.
     /// Items can die and be revived by users with a specific karma threshold.
-    let dead: Bool?
+    public let dead: Bool?
 
     /// The parent of a comment since comments can nest.
-    let parent: Int?
+    public let parent: Int?
 
     /// A poll option's associated poll.
-    let poll: Int?
+    public let poll: Int?
 
     /// The IDs of an item's comments in a ranked display order.
-    let kids: [Int]?
+    public let kids: [Int]?
 
     /// The URL of a story. This can be an article or whatever website.
-    let url: URL?
+    public let url: URL?
 
     /// The score of the item. Users on Hacker News can upvote stories they find intellectual.
-    let score: Int?
+    public let score: Int?
 
     /// The title of a story, poll, or job. It's in HTML.
-    let title: String?
+    public let title: String?
 
     /// A list of related poll options in a display order.
-    let parts: [Int]?
+    public let parts: [Int]?
 
     /// The total comment count for stories and polls.
-    let descendants: Int?
+    public let descendants: Int?
 
     // MARK: Convenience
 
     /// A convenience mirror of `by`.
-    var author: String? {
+    public var author: String? {
         by
     }
 
     /// Mirror of `time` for convenience.
-    var created: Date? {
+    public var created: Date? {
         time
     }
 
     /// A mirror of `text`.
-    var body: String? {
+    public var body: String? {
         text
     }
 
     /// A mirror of `kids`. Simply, the IDs of the item's comments in a ranked display order.
     /// See `kids` for more info.
-    var comments: [Int]? {
+    public var comments: [Int]? {
         kids
     }
 
     /// A mirror of `score` to adhere to more naming schemes.
-    var karma: Int? {
+    public var karma: Int? {
         score
     }
 
     /// Like `karma`, a mirror of `score`.
     /// See `score` for more info.
-    var upvotes: Int? {
+    public var upvotes: Int? {
         score
     }
 
     /// A mirror of `descendants`.
     /// See `descendants` for more info.
-    var commentCount: Int? {
+    public var commentCount: Int? {
         descendants
     }
 
     // MARK: Custom
 
     /// The URL to the item on Hacker News.
-    var hnURL: URL {
+    public var hnURL: URL {
         HNRepo.hnURL.appendingPathComponent("item?id=\(id)")
     }
 
     /// The Open Graph metadata associated with the URL of the story.
     /// This property uses Ogge internally which is a library for fetching Open Graph properties from URLs.
-    var meta: OGObject? {
+    public var meta: OGObject? {
         get async throws {
             guard let url = url else {
                 return nil
