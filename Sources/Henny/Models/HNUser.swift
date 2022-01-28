a811a2bd76df0b5246b18cc9fe062c54f6668f7a
Linus Långberg <git@phoqe.dev>
2022-01-28 15:50:20 +0100

    make public

--- Sources/Henny/Models/HNUser.swift
+++ Sources/Henny/Models/HNUser.swift
@@ -8,48 +8,48 @@ public struct HNUser: Codable {
 
     /// The name might be misleading but the ID is actually the user's username.
     /// It's a required property and case-sensitive.
-    let id: String
+    public let id: String
 
     /// When the user was created.
-    let created: Date
+    public let created: Date
 
     /// How many upvotes the user has received on their submissions.
-    let karma: Int
+    public let karma: Int
 
     /// A user-provided biography where they can enter information about themselves.
-    let about: String?
+    public let about: String?
 
     /// A list of the user's submissions like stories, polls, and comments.
-    let submitted: [Int]?
+    public let submitted: [Int]?
 
     // MARK: Convenience
 
     /// The username property is a convenience mirror of the `id` property.
     /// Usernames are required properties and case-sensitive.
-    var username: String {
+    public var username: String {
         id
     }
 
     /// Mirror of `karma`.
-    var score: Int {
+    public var score: Int {
         karma
     }
 
     /// Mirror of `karma`.
-    var upvotes: Int {
+    public var upvotes: Int {
         karma
     }
 
     /// A convenience property for `submitted`.
     /// For more information, check that property out.
-    var submissions: [Int]? {
+    public var submissions: [Int]? {
         submitted
     }
 
     // MARK: Custom
 
     /// The URL to the user on Hacker News.
-    var hnURL: URL {
+    public var hnURL: URL {
         HNRepo.hnURL.appendingPathComponent("user?id=\(id)")
     }
 }
