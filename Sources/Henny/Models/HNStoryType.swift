0c7f906e9d23e5fde97efca82e5db9710c978723
Linus Långberg <git@phoqe.dev>
2022-01-29 13:25:04 +0100

    Revert "remove more impl specific"
    This reverts commit 1228d696e03fe0ea69f1fb21330883373faeb248.

--- Sources/Henny/Models/HNStoryType.swift
+++ Sources/Henny/Models/HNStoryType.swift
@@ -2,11 +2,15 @@ import Foundation
 
 /// The different types of stories there are on Hacker News.
 /// You can use this enum to query the different endpoints.
-public enum HNStoryType: String, Codable {
+public enum HNStoryType: String, Codable, CaseIterable, Identifiable {
     case top
     case new
     case best
     case ask
     case show
     case job
+
+    public var id: String {
+        self.rawValue
+    }
 }
